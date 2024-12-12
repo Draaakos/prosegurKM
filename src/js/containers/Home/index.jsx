6a3c1b76965ec0f8e595654c322aeaaed5daf91b
Drakos <orlando.andaur.c@gmail.com>
2024-12-12 14:59:24 -0300

    advances in login

--- src/js/containers/Home/index.jsx
+++ src/js/containers/Home/index.jsx
@@ -1,9 +1,30 @@
+import css from './index.css';
+
+
 const HomeApp = () => {
   return (
-    <div>
-      <div>home</div>
+    <div className={css.page}>
+      <div className={css.content}>
+        <div className={css.leftContent}>
+          <div>
+            <div className={css.title}>Bienvenido de vuelta!</div>
+            <div>Para mantenerte conectado con nosotros, inicia sesión con tus datos personales.</div>
+          </div>
+        </div>
+        <div className={css.rightContent}>
+          <div className={css.formContent}>
+            <div className={css.title}>INICIAR SESION</div>
+            <div>
+              <div><input type="text" placeholder="example@gmail.com"/></div>
+              <div><input type="password" placeholder="password"/></div>
+              <div><button>Login</button></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
+
 
 export default HomeApp;
